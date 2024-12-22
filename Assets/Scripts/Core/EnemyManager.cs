226db4b1cd98b6600ca9233ad14aea37c67d8a01
Winness R <winness.rakotozafy@outlook.fr>
2024-12-22 21:50:32 +0100

    feat: add playerBase targetting

--- Assets/Scripts/Core/EnemyManager.cs
+++ Assets/Scripts/Core/EnemyManager.cs
@@ -5,6 +5,7 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] private EnemyFactory walkingEnemyFactory;
     [SerializeField] private EnemyFactory flyingEnemyFactory;
     [SerializeField] private EnemyFactory tankEnemyFactory;
+    [SerializeField] private Transform target;
 
     public void Start()
     {
@@ -15,5 +16,17 @@ public class EnemyManager : MonoBehaviour
         Enemy walkingEnemy = walkingEnemyFactory.CreateEnemy();
         Enemy flyingEnemy = flyingEnemyFactory.CreateEnemy();
         Enemy tankEnemy = tankEnemyFactory.CreateEnemy();
+
+        walkingEnemy.Move(target.position);
+        flyingEnemy.Move(target.position);
+        tankEnemy.Move(target.position);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnEnemy();
+        }
     }
 }
