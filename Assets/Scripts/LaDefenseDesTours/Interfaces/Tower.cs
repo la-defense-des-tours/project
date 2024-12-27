936f937a8b8e1b4aa07e607af4814d330c0236e5
Vladimir9595 <vladisac@hotmail.it>
2024-12-27 21:38:30 +0100

    feat: add abstract class tower

--- Assets/Scripts/LaDefenseDesTours/Interfaces/Tower.cs
+++ Assets/Scripts/LaDefenseDesTours/Interfaces/Tower.cs
@@ -2,71 +2,12 @@ using UnityEngine;
 
 namespace Assets.Scripts.LaDefenseDesTours.Interfaces
 {
-    /// <summary>
-    /// Interface for tower
-    /// </summary>
-    public interface Tower
+    public abstract class Tower
     {
-        /// <summary>
-        /// Gets the tower's range
-        /// </summary>
-        float Range { get; }
-
-        /// <summary>
-        /// Gets the tower's level
-        /// </summary>
-        int currentLevel { get; }
-
-
-
-        /// <summary>
-        /// Gets the tower's damage
-        /// </summary>
-        float Damage { get; }
-
-        /// <summary>
-        /// Gets the tower's fire rate
-        /// </summary>
-        float FireRate { get; }
-
-        /// <summary>
-        /// Gets the tower's cost
-        /// </summary>
-        int Cost { get; }
-
-        /// <summary>
-        /// Gets the tower's upgrade cost
-        /// </summary>
-        int UpgradeCost { get; }
-
-        /// <summary>
-        /// Gets the tower's upgrade damage
-        /// </summary>
-        float UpgradeDamage { get; }
-
-        /// <summary>
-        /// Gets the tower's upgrade fire rate
-        /// </summary>
-        float UpgradeFireRate { get; }
-
-        /// <summary>
-        /// Gets the tower's upgrade range
-        /// </summary>
-        float UpgradeRange { get; }
-
-        /// <summary>
-        /// Gets the tower's upgrade cost
-        /// </summary>
-        int SellValue { get; }
-
-        /// <summary>
-        /// Gets the tower's upgrade cost
-        /// </summary>
-        int UpgradeSellValue { get; }
-
-        /// <summary>
-        /// Gets the tower's upgrade cost
-        /// </summary>
-        void Upgrade();
+        public int level { get; set; }
+        public float health { get; set; }
+        public int price { get; set; }
+        public abstract void Upgrade();
+        public abstract void Attack();
     }
 }
