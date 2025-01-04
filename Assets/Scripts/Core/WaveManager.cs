b5474883e5d7dec89f09585bad4c78b0b0217ff4
Vladimir9595 <vladisac@hotmail.it>
2025-01-04 17:22:30 +0100

    fix: handle the waves spawn by key code

--- Assets/Scripts/Core/WaveManager.cs
+++ Assets/Scripts/Core/WaveManager.cs
@@ -1,5 +1,6 @@
 using Assets.Scripts.LaDefenseDesTours.Interfaces;
 using Assets.Scripts.LaDefenseDesTours.Waves;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Core
@@ -12,33 +13,44 @@ namespace Assets.Scripts.Core
         [SerializeField] private EnemyFactory bossEnemyFactory;
         [SerializeField] private Transform target;
 
-        private Wave waveChain;
+        private Dictionary<KeyCode, Wave> waveKeyBindings;
 
         private void Start()
         {
             SetupWaves();
-            StartWaveSequence();
         }
 
         private void SetupWaves()
         {
-            // Creation du boss
-            Enemy bossEnemy = bossEnemyFactory.CreateEnemy();
-
             // Initialization des vagues
             var wave1 = new Wave_1(walkingEnemyFactory, flyingEnemyFactory, tankEnemyFactory);
             var wave2 = new Wave_2(walkingEnemyFactory, flyingEnemyFactory, tankEnemyFactory);
             var wave3 = new Wave_3(walkingEnemyFactory, flyingEnemyFactory, tankEnemyFactory);
             var wave4 = new Wave_4(walkingEnemyFactory, flyingEnemyFactory, tankEnemyFactory, bossEnemyFactory);
 
-            // Enchainement des vagues
-            waveChain = wave1.SetNext(wave2).SetNext(wave3).SetNext(wave4);
+            waveKeyBindings = new Dictionary<KeyCode, Wave>
+            {
+                { KeyCode.G, wave1 },
+                { KeyCode.H, wave2 },
+                { KeyCode.J, wave3 },
+                { KeyCode.K, wave4 }
+            };
         }
 
-        private void StartWaveSequence()
+        private void Update()
         {
-            Debug.Log("Starting wave sequence...");
-            waveChain?.GenerateWave(target.position);
+            HandleWaveInput();
+        }
+        private void HandleWaveInput()
+        {
+            foreach (var keyWavePair in waveKeyBindings)
+            {
+                if (Input.GetKeyDown(keyWavePair.Key))
+                {
+                    Debug.Log($"Starting wave triggered by {keyWavePair.Key}");
+                    keyWavePair.Value.GenerateWave(target.position);
+                }
+            }
         }
     }
 }
