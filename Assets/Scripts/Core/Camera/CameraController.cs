3baa27ec6e43efbeb5c0e7047e7bd742a5cfc897
Winness R <winness.rakotozafy@outlook.fr>
2025-01-29 22:23:50 +0100

    feat: finish camera controller tweaks to stay inbounds

--- Assets/Scripts/Core/Camera/CameraController.cs
+++ Assets/Scripts/Core/Camera/CameraController.cs
@@ -5,30 +5,25 @@ public class CameraController : MonoBehaviour
     [SerializeField] private bool disableMovement = false;
     [SerializeField] private float panSpeed = 30f;
     [SerializeField] private float scrollSpeed = 5f;
-    [SerializeField] private float minY = 35f;
-    [SerializeField] private float maxY = 75f;
-    [SerializeField] private GameObject mapObject;
+    [SerializeField] private GameObject cameraBoundary;
 
-    private Vector3 moveForward = new(1, 0, 0);
-    private Vector3 moveBackward = new(-1, 0, 0);
-    private Vector3 moveLeft = new(0, 0, 1);
-    private Vector3 moveRight = new(0, 0, -1);
-    private Vector3 mapMinBounds;
-    private Vector3 mapMaxBounds;
+    private Camera cam;
+    private Bounds mapBounds;
     private bool doMovement = true;
+    private readonly float minZoom = 35f;
+    private readonly float maxZoom = 75f;
+    private readonly Vector3 forward = new(1, 0, 0);
+    private readonly Vector3 back = new(-1, 0, 0);
+    private readonly Vector3 left = new(0, 0, 1);
+    private readonly Vector3 right = new(0, 0, -1);
 
     void Start()
     {
-        if (mapObject.TryGetComponent(out Collider collider))
-        {
-            Bounds bounds = collider.bounds;
-            mapMinBounds = bounds.min;
-            mapMaxBounds = bounds.max;
-        }
+        cam = Camera.main;
+        if (cameraBoundary.TryGetComponent(out Collider collider))
+            mapBounds = collider.bounds;
         else
-        {
             Debug.LogError("Map Object must have a Collider to calculate boundaries.");
-        }
     }
 
     void Update()
@@ -49,38 +44,53 @@ public class CameraController : MonoBehaviour
         HandleCameraZoom();
     }
 
-    void MoveCamera(Vector3 direction)
-    {
-        transform.Translate(panSpeed * Time.deltaTime * direction, Space.World);
-
-        Vector3 pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, mapMinBounds.x, mapMaxBounds.x);
-        pos.z = Mathf.Clamp(pos.z, mapMinBounds.z, mapMaxBounds.z);
-        transform.position = pos;
-    }
-
     void HandleCameraMovement()
     {
+        Vector3 moveDirection = Vector3.zero;
+
         if (Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - 10)
-            MoveCamera(moveForward);
+            moveDirection += forward;
         if (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= 10)
-            MoveCamera(moveBackward);
+            moveDirection += back;
         if (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= 10)
-            MoveCamera(moveLeft);
+            moveDirection += left;
         if (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - 10)
-            MoveCamera(moveRight);
+            moveDirection += right;
+
+        if (moveDirection != Vector3.zero)
+            MoveCamera(moveDirection.normalized);
+    }
+
+    void MoveCamera(Vector3 direction)
+    {
+        Vector3 targetPosition = transform.position + panSpeed * Time.deltaTime * direction;
+        targetPosition = ClampCameraPosition(targetPosition);
+        transform.position = targetPosition;
+    }
+
+    Vector3 ClampCameraPosition(Vector3 targetPosition)
+    {
+        float halfHeight = cam.orthographicSize;
+        float halfWidth = halfHeight * cam.aspect;
+
+        float x = Mathf.Clamp(targetPosition.x, mapBounds.min.x + halfHeight, mapBounds.max.x - halfHeight);
+        float z = Mathf.Clamp(targetPosition.z, mapBounds.min.z + halfWidth, mapBounds.max.z - halfWidth);
+
+        targetPosition = new Vector3(x, targetPosition.y, z);
+
+        return targetPosition;
     }
 
     void HandleCameraZoom()
     {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        Vector3 pos = transform.position;
-
-        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        transform.position = pos;
+        if (Mathf.Abs(scroll) > 0.01f)
+        {
+            Vector3 pos = transform.position;
+            pos.y = Mathf.Clamp(pos.y - scroll * 1000 * scrollSpeed * Time.deltaTime, minZoom, maxZoom);
+            transform.position = ClampCameraPosition(pos);
+        }
     }
-
     void DebugCameraInfo()
     {
         Debug.Log(doMovement ? "Camera movement enabled" : "Camera movement disabled");
