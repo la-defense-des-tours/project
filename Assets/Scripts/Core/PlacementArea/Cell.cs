5bff76cc0bbe45ff1b614d6074729314bb92849d
Winness R <winness.rakotozafy@outlook.fr>
2025-02-11 22:10:46 +0100

    feat: add cell prefab material

--- Assets/Scripts/Core/PlacementArea/Cell.cs
+++ Assets/Scripts/Core/PlacementArea/Cell.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    [SerializeField] private Color hoverColor;
+    [SerializeField] private Material hoverMaterial;
     private Renderer cellRenderer;
+    private Material defaultMaterial;
     private void Start()
     {
         cellRenderer = GetComponent<Renderer>();
-
+        defaultMaterial = cellRenderer.material;
     }
 
     private void OnMouseEnter()
     {
-        cellRenderer.material.color = hoverColor;
+        cellRenderer.material = hoverMaterial;
     }
 
     private void OnMouseExit()
     {
-        Debug.Log("Exit Cell");
+        cellRenderer.material = defaultMaterial;
     }
 }
