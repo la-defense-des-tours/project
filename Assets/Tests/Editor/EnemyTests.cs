c5aa938b4d1c7909cd5813326fbb21b5c2e2788b
Vladimir9595 <vladisac@hotmail.it>
2025-02-19 22:56:30 +0100

    fix: change expected player health value

--- Assets/Tests/Editor/EnemyTests.cs
+++ Assets/Tests/Editor/EnemyTests.cs
@@ -127,7 +127,7 @@ public class EnemyTests
     public void DealDamage_CallsPlayerTakeDamageWithCorrectValue()
     {
         enemy.DealDamage(200f);
-        double expectedPlayerHealth = 700;
+        double expectedPlayerHealth = 800;
         double resultPlayerHealth = Player.GetInstance().health;
 
         Assert.AreEqual(expectedPlayerHealth, resultPlayerHealth);
