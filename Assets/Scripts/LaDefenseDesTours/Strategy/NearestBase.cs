b37d381abb514d01fcd192713569c1d21decfbf5
Winness R <winness.rakotozafy@outlook.fr>
2025-03-08 20:54:33 +0100

    feat: clean out decorators & update NearestBase for early return

--- Assets/Scripts/LaDefenseDesTours/Strategy/NearestBase.cs
+++ Assets/Scripts/LaDefenseDesTours/Strategy/NearestBase.cs
@@ -7,6 +7,7 @@ namespace LaDefenseDesTours.Strategy
     public class NearestBase : IStrategy
     {
         private readonly string ENEMY_TAG = "Enemy";
+
         public string GetStrategyName()
         {
             return "Nearest Base";
@@ -14,21 +15,33 @@ namespace LaDefenseDesTours.Strategy
 
         public Enemy SelectTarget(Enemy[] enemies, Vector3 towerPosition, float range)
         {
+
+            if (enemies == null || enemies.Length == 0)
+                return null;
+
             Enemy selected = null;
             float shortestDistanceToBase = Mathf.Infinity;
-            
+            Vector3 basePosition = LevelManager.instance.GetEnemyEndPoint();
+
             foreach (Enemy enemy in enemies)
             {
-                float distance = Vector3.Distance(towerPosition, enemy.transform.position);
-                float distanceToBase = Vector3.Distance(enemy.transform.position, LevelManager.instance.GetEnemyEndPoint());
-                if (distance <= range && distanceToBase < shortestDistanceToBase)
+                if (enemy == null || !enemy.gameObject.activeInHierarchy || !enemy.CompareTag(ENEMY_TAG))
+                    continue;
+
+                float distanceToEnemy = Vector3.Distance(towerPosition, enemy.transform.position);
+
+                if (distanceToEnemy > range)
+                    continue;
+
+                float distanceToBase = Vector3.Distance(enemy.transform.position, basePosition);
+                if (distanceToBase < shortestDistanceToBase)
                 {
                     shortestDistanceToBase = distanceToBase;
                     selected = enemy;
                 }
             }
 
-            return selected != null && selected.gameObject.CompareTag(ENEMY_TAG) ? selected : null;
+            return selected;
         }
     }
 }
