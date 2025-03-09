01e6699ae0cee06f7c06fb8ba1798c4ebb7c30d7
Winness R <winness.rakotozafy@outlook.fr>
2025-03-09 14:26:31 +0100

    feat: extend rotator for satellite and use star skybox newMenu

--- Assets/Scripts/LaDefenseDesTours/UI/Rotator.cs
+++ Assets/Scripts/LaDefenseDesTours/UI/Rotator.cs
@@ -1,17 +1,44 @@
 ﻿using UnityEngine;
 
-namespace TowerDefense.UI
+namespace LaDefenseDesTours.UI
 {
-	/// <summary>
-	/// A simple component that applies a constant rotation to a transform
-	/// </summary>
-	public class Rotator : MonoBehaviour
-	{
-		public Vector3 rotationSpeed;
-	
-		void Update ()
-		{
-			transform.localEulerAngles += rotationSpeed;
-		}
-	}
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private Vector3 rotationSpeed;
+        [SerializeField] private bool clampRotation = false;
+        [SerializeField] private Vector3 minRotation;
+        [SerializeField] private Vector3 maxRotation;
+
+        private Vector3 currentEulerAngles;
+
+        private void Start()
+        {
+            currentEulerAngles = transform.localEulerAngles;
+        }
+
+        private void Update()
+        {
+            currentEulerAngles += rotationSpeed * Time.deltaTime;
+
+            if (clampRotation)
+                currentEulerAngles = ClampEulerAngles(currentEulerAngles);
+
+            transform.localEulerAngles = currentEulerAngles;
+        }
+
+        private Vector3 ClampEulerAngles(Vector3 angles)
+        {
+            return new Vector3(
+                Mathf.Clamp(NormalizeAngle(angles.x), minRotation.x, maxRotation.x),
+                Mathf.Clamp(NormalizeAngle(angles.y), minRotation.y, maxRotation.y),
+                Mathf.Clamp(NormalizeAngle(angles.z), minRotation.z, maxRotation.z)
+            );
+        }
+
+        private float NormalizeAngle(float angle)
+        {
+            angle %= 360f;
+            return angle < 0f ? angle + 360f : angle;
+        }
+    }
 }
